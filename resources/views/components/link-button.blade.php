e03f530c11bd7a7db57e1978414ac30ee2cd0d2c
Md. Mesbaul Islam <mesbaul.cse26@gmail.com>
2025-08-09 17:36:28 +0600

    delete unused file

--- resources/views/components/link-button.blade.php
+++
@@ -1,3 +0,0 @@
-<a {{ $attributes->merge(['class' => 'inline-block items-center px-3 py-3 rounded-lg font-semibold text-xs text-center transition ease-in-out duration-150 border']) }}>
-    {{ $slot }}
-</a>
